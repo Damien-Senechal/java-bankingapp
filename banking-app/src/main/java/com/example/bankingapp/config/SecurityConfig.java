18d59bafb719c50b925e9159732d61419fe46eca
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2024-07-24 03:29:45 +0200

    le projet est cassé :(

--- banking-app/src/main/java/com/example/bankingapp/config/SecurityConfig.java
+++ banking-app/src/main/java/com/example/bankingapp/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.bankingapp.config;
 
-import org.hibernate.annotations.CollectionTypeRegistration;
+import com.example.bankingapp.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,15 +18,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Nouvelle syntaxe pour désactiver CSRF
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/**").permitAll() // Permettre l'accès sans authentification pour les tests
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
                 )
-                .httpBasic(withDefaults()); // Utilise l'authentification basique HTTP
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
 
         return http.build();
     }
